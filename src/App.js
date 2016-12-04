a329b9b2b76deddca27092a46b2fc47c0888e413
superce <562923778@qq.com>
2016-12-04 13:42:53 +0800

    wancheng

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import React from 'react';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import NavHeader from './component/NavHeader';
 import NavFooter from './component/NavFooter';
+import LeftNav from './component/LeftNav';
 
 class App extends React.Component {
   constructor(){
     super();
-    this.state={title:'Home'}
+    this.state={
+      title:'Home',
+      showLeftNav:false
+    }
   }
   getChildContext() {
     return {muiTheme: getMuiTheme()};
@@ -17,24 +21,56 @@ class App extends React.Component {
   }
   componentWillMount(){
     this.setTitle();
+    if(window.innerWidth>760){
+      this.setState({showLeftNav:true})
+    }
+  }
+  componentDidMount(){
+    window.onresize=this.setNavBar.bind(this)
+  }
+  setNavBar(){
+    this.setState({
+      showLeftNav:window.innerWidth>760 ? true : false
+    })
+  }
+
+  componentWillMount(){
+    let _this = this;
+
+    window.onresize = function(){
+      if(window.innerWidth>760){
+      _this.setState({
+        showLeftNav:true
+      })
+
+
+    }else {
+       _this.setState({
+         showLeftNav:false
+       })
+    }
+  }
   }
   setTitle(){
     this.setState({
       title:this.props.router.isActive('/',true) ? 'Home' :
       this.props.router.isActive('blog') ? 'Blog' :
-      this.props.router.isActive('work') ? 'Work' : 'About'
+      this.props.router.isActive('work') ? 'Work' :
+      this.props.router.isActive('about') ? 'About' : 'Blog'
     })
   }
   render () {
     return(
       <div className='my-wrap'>
-        <NavHeader title={this.state.title}/>
+        {this.state.showLeftNav ? <LeftNav title={this.state.title}/> : <NavHeader title={this.state.title}/>}
+      
+
 
         <div className='main'>
           {this.props.children}
         </div>
+        {this.state.showLeftNav ? null : <NavFooter />}
 
-        <NavFooter />
       </div>
     )
   }
--- src/Item/index.js
+++ src/Item/index.js
@@ -1,18 +1,36 @@
 import React, { PropTypes } from 'react'
 import marked from 'marked';
 import axios from 'axios';
+import hljs from 'highlight.js'
 class Item extends React.Component {
+  constructor(){
+    super();
+    this.state={
+      data:""
+    }
+  }
   componentDidMount(){
     let address = this.props.params.title;
     axios.get('')
+    axios.get(
+      `https://raw.githubusercontent.com/superce/demo-09/master/data/${address}.md`)
+        .then(res =>this.Setstate({data:res.data}) )
+        .catch(err => alert(err))
+
   }
   render () {
-
+       let content = this.state.data.length==0 ? 'loading' : marked(this.state.data);
+       marked.setOptions({
+        highlight: function (code) {
+          return hljs.highlightAuto(code).value;
+        }
+      });
     return(
       <div>
-         {content}
+        <div className='post-wrap' dangerouslySetInnerHTML={{__html:content}} />
 
-          <div dangerouslySetInnerHTML={{__html:marked(`# ssss`)}} />
+
+
       </div>
     )
 
